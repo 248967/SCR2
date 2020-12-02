7d7409343101b81b1fe1e0753541c5ca54baa655
Izabela Kozlowska <248967@student.pwr.edu.pl>
2020-12-02 19:51:04 +0100

    Lab5_zad1_v2

--- Lab5/zad1.c
+++ Lab5/zad1.c
@@ -21,6 +21,8 @@ int main (int argc, char **argv)
 	FILE *stream;									// wskaźnik do użycia do czytania potoku
 	char bufor[5];  								//bufor do czytania paczkami
 
+	 size_t bytes_read; 							 //używany do sprawdzenia ilości odczytanych znaków z potoku
+
 
 
 	// Stworzenie potoku
@@ -33,18 +35,39 @@ int main (int argc, char **argv)
 	// Stworzenie procesu potomnego
 	pid = fork ();
 
-	if (pid == (pid_t) 0)				// jeśli proces potomny
+	if (pid == (pid_t) 0)										// jeśli proces potomny
 	{
-		return 0;
-	}
-	else if (pid < (pid_t) 0)			// nieudało się stworzyć procesu potomnego
-	{
-		fprintf (stderr, "Nie udało się stworzyć procesu potomnego.\n");
-		return -1;
-	}
-	else								// jeśli rodzic
-	{
-		return 0;
+	      //proces potomny zamknij potok do pisania, otwórz do czytania
+	      close (potok[1]);
+		  stream = fdopen (potok[0], "r");
 
+	      //czytaj paczkami po 5 znaków dodawaj na początku i końcu każdej # i wypisz na stdout
+	      while ((bytes_read = fread(bufor, sizeof(char), 5, stream)) > 0)
+	      {
+	    	putchar ('#');										//wypisanie # na stdout na początku buforu
+	        for(int i=0; i<5; i++)
+	        {
+	          printf("%c", bufor[i]);
+	        }
+	        putchar ('#');										//wypisanie # na stdout na końcu buforu
+	        //czyszczenie bufora, żeby przypadkowo nie wypisać poprzednich wartości
+	        for(int i=0; i<5; i++)
+	        {
+	          bufor[i]=NULL;
+	        }
+	      }
+	      fclose(stream);
+
+	    return 0;
 	}
-}
+		else if (pid < (pid_t) 0)			// nieudało się stworzyć procesu potomnego
+		{
+			fprintf (stderr, "Nie udało się stworzyć procesu potomnego.\n");
+			return -1;
+		}
+		else								// jeśli rodzic
+		{
+			return 0;
+
+		}
+	}
