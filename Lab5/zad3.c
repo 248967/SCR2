bd84e04a5dcc502e6dff10ee49298164bb633fc5
Izabela Kozlowska <248967@student.pwr.edu.pl>
2020-12-02 22:25:21 +0100

    Lab5_zad3_v2

--- Lab5/zad3.c
+++ Lab5/zad3.c
@@ -10,41 +10,53 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 
 int main (int argc, char **argv)
 {
-  FILE *stream_fifo;
+	int c;
+	FILE *stream_fifo;
 
-  // Stworzenie potoku FIFO
-  char *myfifo = "/tmp/myfifo";	//ścieżka dla FIFO
-  if (mkfifo(myfifo, 0666))			// stworzenie fifo, nadanie uprawnień
-  {
-    fprintf (stderr, "Nie udało się stworzyć FIFO\n");
-    return -1;
-  }
+	// Stworzenie potoku FIFO
+	char *myfifo = "/tmp/myfifo";	//ścieżka dla FIFO
+	if (mkfifo(myfifo, 0666)==-1)			// stworzenie fifo, nadanie uprawnień
+	{
+		fprintf (stderr, "Nie udało się stworzyć FIFO\n");
+		return -1;
+	}
 
-  if (argc < 2)
-  {
-	fprintf (stderr, "Użycie: %s nazwy_plików\n", argv[0]);
-	return -1;
-  }
+	if (argc < 2)
+	{
+		fprintf (stderr, "Użycie: %s nazwy_plików\n", argv[0]);
+		return -1;
+	}
 
-  FILE *plikwej[argc];	//deklaracja tablicy wskaźników plików wejściowych
+	FILE *plikwej[argc-1];	//deklaracja tablicy wskaźników plików wejściowych
 
-  for (int i = 1; i < argc; i++)
-  {
-  	//otwarcie pliku do czytania
-	plikwej[i] = fopen ( argv [i], "r");
-   	if ( plikwej[i] == NULL )
-   	{
-   	  printf ( "Brak pliku\n" );
-   	  return -1;
-   	}
+	for (int i = 1; i < argc; i++)
+	{
+		//otwarcie pliku do czytania
+		plikwej[i-1] = fopen ( argv [i], "r");
+		if ( plikwej[i] == NULL )
+		{
+			printf ( "Brak pliku\n" );
+			return -1;
+		}
+
+		stream_fifo = fopen(myfifo, "w");  //otworzenie fifo do zapisu
+		//czytam po kolei znaki z pliku wejściowego i zapisuje do potoku FIFO
+		while ((c = fgetc (plikwej[i])) != EOF )
+		{
+			fprintf (stream_fifo, "%c", c);
+			//fprintf (stderr, "\n %c \n", c);
+		}
+
+		fclose(stream_fifo);
 
 
-    fclose(plikwej[i]);
-  }
+		fclose(plikwej[i]);
+	}
 
-   return 0;
+	return 0;
 }
