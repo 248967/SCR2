d8ef25a4267bfbd4193be6c9d8cf40a1b3fc5c71
Izabela Kozlowska <248967@student.pwr.edu.pl>
2020-12-16 21:20:55 +0100

    Lab6_zad2_v1

---
+++ Lab6/zad2.c
@@ -0,0 +1,108 @@
+/*
+ * zad2.c
+ *
+ *  Created on: 16 gru 2020
+ *      Author: izabela
+ */
+
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main ()
+{
+	int fd, fdw;
+	int *map;
+	char filename[50]; //nazwa wczytanego pliku w pętli
+	struct stat sb;	//używane do sprawdzenia rozmiaru pliku
+	long int filesize; //zmienna do rozmiaru pliku
+
+	//otwórz plik do czytania i pisania, jesli nie istnieje stwórz go, jesli istnieje obetnij go
+	fd = open("/tmp/plikdomapowania", O_RDWR | O_CREAT | O_TRUNC, (mode_t)0666);
+	if (fd == -1)
+	{
+		printf("Błąd otwarcia pliku\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//mapowanie pliku - odczyt zapis, inicjalny rozmiar 4096
+	map = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED)
+	{
+		close(fd);
+		printf("Błąd mapowania pliku\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//głowna petla
+	while(1)
+	{
+		//otwarcie pliku podanego przez użytkownika
+		printf("Podaj nazwę pliku: ");
+		scanf("%s", filename);
+		fdw = open(filename, O_RDONLY);
+		if (fdw == -1)
+		{
+			printf("Błąd otwarcia pliku\n");
+			exit(EXIT_FAILURE);
+		}
+
+		// sprawdzenie rozmiaru pliku
+		if (stat(filename, &sb) == -1)
+		{
+			printf("Nie udało się pobrać rozmiaru pliku\n");
+			exit(EXIT_FAILURE);
+		}
+		filesize = sb.st_size;
+
+		//zmiana wielkości pliku zmapowanego wyściowego fd
+		if (ftruncate(fd,filesize) == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		//printf("Debug mmap zmiana\n");
+
+		//mapowanie otwartego pliku fd do pamięci
+		map = mmap(0, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		if (map == MAP_FAILED)
+		{
+			close(fdw);
+			printf("Błąd mapowania pliku wejściowego\n");
+			exit(EXIT_FAILURE);
+		}
+
+		//printf("Debug kopiowanie do pamięci");
+
+		//kopiowanie pliku wejściowego do pamięci
+		int num = read(fdw, map, filesize);
+		//printf ("Debug ilość bytów: %d \n", num);
+
+		//gets
+		//stat
+		//truncate
+		//addr=mmap
+		//fd=open()
+		//read(fd,addr)
+		//msync(addr,)
+
+	}
+
+	// zwolnienie zmapowanej pamięci i zamknięcie plików - program tu teorytycznie nie wejdzie, ale zostawiam dla pewności
+	if (munmap(map, filesize) == -1)
+	{
+		printf("Błąd odmapowania pliku");
+	}
+
+
+	close(fd);
+	close(fdw);
+	return 0;
+
+}
