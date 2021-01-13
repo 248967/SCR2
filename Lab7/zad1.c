c61688a0cfe1b5f1d78d82a1c97c6cfbbd67b667
Izabela Kozlowska <248967@student.pwr.edu.pl>
2021-01-13 19:43:20 +0100

    Lab7_zad1_gotowe_248967

--- Lab7/zad1.c
+++ Lab7/zad1.c
@@ -5,6 +5,8 @@
  *      Author: izabela
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 // funkcja wyświetlająca napis
@@ -14,12 +16,27 @@ void *HelloSCR(void *threadid)
 	int tid = *((int*)threadid);
 	//wyświetlenie tekstu
 	printf("Hello SCR. Written by thread %d \n", tid);
+	//zakończenie wątku
 	pthread_exit(NULL);
 }
 
 int main ()
 {
-
+	pthread_t threads[5];
+	int indexes[5];
+	int i;
+	// tworzenie 5 wątków
+	for( i=0; i < 5; i++ )
+	{
+		indexes[i] = i;
+	    int result = pthread_create(&threads[i], NULL, HelloSCR, (void *)&(indexes[i]));
+	    if (result !=0)
+	    {
+	         printf("Nie udało się utworzyć wątku.\n");
+	    	 exit(-1);
+	    }
+	}
+	pthread_exit(NULL);
 }
 
 
